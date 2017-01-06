738412b1428681268217222ede79119e3a8517e5
Виктор Гребень < >
2017-01-07 01:10:26 +0300

    Добавил капчу и сохранение в БД

--- application/modules/register/controllers/IndexController.php
+++ application/modules/register/controllers/IndexController.php
@@ -6,6 +6,19 @@ class Register_Form_Index extends Zend_Form {
     // инициализируем форму
     $this->setAction('/register')->setMethod('post');
 
+    // создаем текстовое поле для организации
+    $customer = new Zend_Form_Element_Text('customer');
+    $customer -> setLabel('Организация')
+        ->setAttribs(array(
+            'class' => 'form-control',
+            'placeholder'  => 'Укажите название организации',
+        ))
+        -> setOptions(array('size' => '35'))
+        -> setRequired(true)
+        -> addValidator('NotEmpty', false)
+        -> addFilter('HtmlEntities')
+        -> addFilter('StringTrim');
+    
     // создаем текстовое поле для ввода названия
     $family = new Zend_Form_Element_Text('family');
     $family -> setLabel('Фамилия')
@@ -15,7 +28,7 @@ class Register_Form_Index extends Zend_Form {
         ))
         -> setOptions(array('size' => '35'))
         -> setRequired(true)
-        -> addValidator('NotEmpty', true)
+        -> addValidator('NotEmpty', false)
         -> addFilter('HtmlEntities')
         -> addFilter('StringTrim');
 
@@ -28,7 +41,7 @@ class Register_Form_Index extends Zend_Form {
         ))
         -> setOptions(array('size' => '35'))
         -> setRequired(true)
-        -> addValidator('NotEmpty', true)
+        -> addValidator('NotEmpty', false)
         -> addFilter('HtmlEntities')
         -> addFilter('StringTrim');
 
@@ -41,7 +54,7 @@ class Register_Form_Index extends Zend_Form {
         ))
         -> setOptions(array('size' => '35'))
         -> setRequired(true)
-        -> addValidator('NotEmpty', true)
+        -> addValidator('NotEmpty', false)
         -> addFilter('HtmlEntities')
         -> addFilter('StringTrim');
 
@@ -60,10 +73,10 @@ class Register_Form_Index extends Zend_Form {
 
     // создаем текстовое поле для ввода адреса электронной почты
     $email = new Zend_Form_Element_Text('email');
-    $email -> setLabel('Электропочта')
+    $email -> setLabel('Электронная почта')
         ->setAttribs(array(
             'class' => 'form-control',
-            'placeholder'  => 'Укажите электронную почту',
+            'placeholder'  => 'Укажите адрес email',
         ))
         -> setOptions(array('size' => '50'))
         -> setRequired(true)
@@ -73,11 +86,35 @@ class Register_Form_Index extends Zend_Form {
         -> addFilter('StringToLower')
         -> addFilter('StringTrim');
 
+    $note = new Zend_Form_Element_Textarea('note');
+    $note->setLabel('Дополнительная информация')
+        ->setAttribs(array(
+            'class' => 'form-control',
+            'rows'  => '3',
+            'placeholder'  => 'Раскажите о себе',
+        ));
+    
     $image = new Zend_Form_Element_File('image');
-    $image->setLabel('Скан')
+    $image->setLabel('Отсканированный бланк заявления')
         ->setDestination(APPLICATION_PATH . '/../public/upload')
+        ->setAttrib('multiple', false)
         ->addValidator('Size', false, 1024000)
         ->addValidator('Extension', false, 'jpg,png,gif');
+    
+    // Капча
+    $captcha = new Zend_Form_Element_Captcha('captcha', array(
+        'captcha' => 'Figlet',
+        'captchaOptions' => array(
+            'captcha' => 'Figlet',
+            'wordLen' => 6,
+            'timeout' => 300,
+        ),
+    ));
+    $captcha->setLabel('Пожалуйста подтвердите что вы человек')
+        ->setAttribs(array(
+            'class' => 'form-control',
+            'placeholder'  => 'Введите текст с картинки',
+        ));
 
     // создаем кнопку отправки
     $submit = new Zend_Form_Element_Submit('submit');
@@ -85,16 +122,19 @@ class Register_Form_Index extends Zend_Form {
         -> setAttribs(array('class' => 'btn btn-success'));
 
     // добавляем элементы к форме
-    $this -> addElement($family)
+    $this -> addElement($customer)
+        -> addElement($family)
         -> addElement($name)
         -> addElement($name2)
         -> addElement($phone)
         -> addElement($email)
+        -> addElement($note)
         -> addElement($image);
 
-    $this->addDisplayGroup(array('family', 'name', 'name2', 'phone', 'email', 'image'), 'zayavka');
+    $this->addDisplayGroup(array('customer', 'family', 'name', 'name2', 'phone', 'email', 'note', 'image'), 'zayavka');
     $this->getDisplayGroup('zayavka')
-        ->setLegend('Заявка на вступление');
+        ->setLegend('Заявление на вступление');
+    $this->addElement($captcha);
     $this->addElement($submit);
   }
 }
@@ -109,7 +149,6 @@ class Register_IndexController extends Zend_Controller_Action
 
     public function indexAction()
     {
-        // action body
         // генерируем форму ввода
         $form = new Register_Form_Index();
         $this->view->form = $form;
@@ -129,6 +168,8 @@ class Register_IndexController extends Zend_Controller_Action
                         $this->buildBody($form->getValues()), 
                         'Скан' . '.' . $originalFilename['extension'], 
                         APPLICATION_PATH . '/../public/upload/' . $newFilename);
+                // Сохраняем в базу
+                $this->saveStatement($form->getValues(), $newFilename);
                 // Перенаправляем
                 $this->_redirect('/register/success');
             }
@@ -171,17 +212,46 @@ class Register_IndexController extends Zend_Controller_Action
     private function buildBody($values)
     {
         $domDoc = new DOMDocument();
-        $title = $domDoc->createElement( 'h2', trim(trim($values['family']. ' ' . $values['name']) . ' ' . $values['name2'])  );
+        $title = $domDoc->createElement( 'h2', $values['customer'] );
+        $person = $domDoc->createElement( 'h3', trim(trim($values['family']. ' ' . $values['name']) . ' ' . $values['name2']) );
         $phone = $domDoc->createElement( 'p', 'Тел.: ' );
         $phone->appendChild( new DOMElement( 'b', $values['phone'] ) );
         $email = $domDoc->createElement( 'p', 'Email: ' );
         $email->appendChild( new DOMElement( 'b', $values['email'] ) );
+        $note = $domDoc->createElement( 'p', $values['note'] );
 
         $domDoc->appendChild($title);
+        $domDoc->appendChild($person);
         $domDoc->appendChild($phone);
         $domDoc->appendChild($email);
+        $domDoc->appendChild( new DOMElement( 'br' ) );
+        $domDoc->appendChild($note);
 
         return $domDoc->saveHTML();
+    }
+    
+    private function saveStatement($values, $file)
+    {
+        // Подключаемся к БД
+        $db = Zend_Db::factory('Pdo_Mysql', array(
+            'host'     => '127.0.0.1',
+            'username' => 'root',
+            'password' => '123',
+            'dbname'   => 'webshop'
+        ));
+        // Формируем массив данных
+        $data = array(
+            'email'     => $values['email'],
+            'fullname'  => $values['customer'],
+            'family'    => $values['family'],
+            'name'      => $values['name'],
+            'name2'     => $values['name2'],
+            'phone'     => $values['phone'],
+            'note'      => $values['note'],
+            'filename'  => $file
+        );
+        // Сохраняем данные
+        $db->insert('statements', $data);
     }
 
 }
