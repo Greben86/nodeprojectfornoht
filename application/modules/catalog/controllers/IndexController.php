6b23172d04e42e57a99198334a5b3037b84b2f22
Victor Greben <grebenvictor@yandex.ru>
2017-10-06 17:32:55 +0300

    Merge origin/development/inwork into development/inwork
    Conflicts:
    	application/layouts/master.phtml
    	application/modules/payment/views/scripts/index/index.phtml

--- application/modules/catalog/controllers/IndexController.php
+++ application/modules/catalog/controllers/IndexController.php
@@ -3,17 +3,17 @@
 class Catalog_IndexController extends Zend_Controller_Action
 {
     public function indexAction()
-    {        
+    {
         // Делаем запрос к API
-        $ch = curl_init(); 
-        curl_setopt($ch, CURLOPT_URL, 'http://127.0.0.1:8080/shop/goods/catalog'); 
-        curl_setopt($ch, CURLOPT_HEADER, false); 
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); 
-        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 30); 
-        curl_setopt($ch, CURLOPT_USERAGENT, 'sodeystvie'); 
-        $result = curl_exec($ch); 
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, 'http://127.0.0.1:8080/shop/goods/catalog');
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 30);
+        curl_setopt($ch, CURLOPT_USERAGENT, 'sodeystvie');
+        $result = curl_exec($ch);
         curl_close($ch);
-        
-        $this->view->goods = json_decode($result, true);        
+
+        $this->view->goods = json_decode($result, true);
     }
 }
