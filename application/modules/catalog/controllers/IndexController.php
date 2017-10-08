973db7a1e4b6d37aa6f179c3c6d7fdb8b830b87c
Виктор <grebenvictor@yandex.ru>
2017-10-08 13:21:51 +0300

    небольшие изменения

--- application/modules/catalog/controllers/IndexController.php
+++ application/modules/catalog/controllers/IndexController.php
@@ -5,6 +5,7 @@ class Catalog_IndexController extends Zend_Controller_Action
     public function indexAction()
     {
         // Делаем запрос к API
+<<<<<<< Updated upstream
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, 'http://127.0.0.1:8080/shop/goods/catalog');
         curl_setopt($ch, CURLOPT_HEADER, false);
@@ -12,6 +13,15 @@ class Catalog_IndexController extends Zend_Controller_Action
         curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 30);
         curl_setopt($ch, CURLOPT_USERAGENT, 'sodeystvie');
         $result = curl_exec($ch);
+=======
+        $ch = curl_init(); 
+        curl_setopt($ch, CURLOPT_URL, 'http://localhost:8080/shop/goods/catalog'); 
+        curl_setopt($ch, CURLOPT_HEADER, false); 
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); 
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 30); 
+        curl_setopt($ch, CURLOPT_USERAGENT, 'sodeystvie'); 
+        $result = curl_exec($ch); 
+>>>>>>> Stashed changes
         curl_close($ch);
 
         $this->view->goods = json_decode($result, true);
