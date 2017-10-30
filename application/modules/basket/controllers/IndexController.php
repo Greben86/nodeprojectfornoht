7d962717ba0de96ee31e96c1ab73d1c283801bb8
Victor Greben <grebenvictor@yandex.ru>
2017-10-30 17:51:42 +0300

    Merge origin/development/inwork into development/inwork
    Conflicts:
    	application/layouts/master.phtml
    	application/modules/payment/views/scripts/index/index.phtml

---
+++ application/modules/basket/controllers/IndexController.php
@@ -0,0 +1,30 @@
+<?php
+
+class Basket_IndexController extends Zend_Controller_Action
+{
+    public function indexAction()
+    {
+        //
+    }
+    
+    public function vidjetAction()
+    {
+        // отключение макета для данного действия
+        $this->_helper->layout->disableLayout(true);
+    }
+    
+    public function addAction()
+    {
+        //
+    }
+    
+    public function updateAction()
+    {
+        //
+    }
+    
+    public function removeAction() 
+    {
+        //
+    }
+}
